6284fdde8ce590c9fa25f00bc5734924538f9af5
None <cutspliceandplay@gmail.com>
2023-03-12 20:16:49 -0400

    fix config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Chattr'
+copyright = '2023, Cloud 8.5'
+author = 'Cloud 8.5'
 
 release = '0.1'
 version = '0.1.0'
